0511c7a1e5bd2275e7647a468330c3241d77f822
gelilaa <gelila.ali@outlook.com>
2021-12-21 17:05:08 +0100

    data added

---
+++ src/data.js
@@ -0,0 +1,26 @@
+const obj = [
+  {
+    Id: 2,
+    Subject: "Workshop-HTML-CSS",
+    StartTime: new Date(2022, 0, 16, 10, 0),
+    EndTime: new Date(2022, 0, 16, 12, 0),
+    IsAllDay: false,
+  },
+  {
+    Id: 3,
+    Subject: "date now",
+    StartTime: new Date(2015, 1, 11, 9, 0),
+    EndTime: new Date(2015, 1, 11, 10, 0),
+    IsAllDay: false,
+  },
+  {
+    Id: 4,
+    Subject: "Vacation",
+    StartTime: new Date(2018, 1, 13, 9, 0),
+    EndTime: new Date(2018, 1, 13, 10, 0),
+    IsAllDay: false,
+  },
+];
+
+
+export default obj
